1deb28dcbae0ea9f03cecf1d5cfc3bc3e426bd00
lkho <llhmtc@gmail.com>
2016-05-02 23:03:00 +0800

    bad description

--- core/src/main/java/org/jdeferred/CancellationCallback.java
+++ core/src/main/java/org/jdeferred/CancellationCallback.java
@@ -9,8 +9,8 @@ public interface CancellationCallback
     /**
      * Called when cancellation is requested by promise. Deferred can decided
      * whether to honour the request or ignore if it is not cancellable.
-     * However it is better to use the normal non-cancellable {@link Deferred} by
-     * confirming to the contract.
+     * However it is better to use the normal non-cancellable {@link Deferred} to
+     * confine to the contract.
      *
      * @param interrupt whether to interrupt forcefully
      */
